682651b9f733bd713d11626449a7f49839dc6edb
Jacob Wilcox <jacob.wilcox@outlook.com>
2021-09-02 09:43:54 -0500

    add working allUser.map

--- client/src/components/person.jsx
+++ client/src/components/person.jsx
@@ -4,21 +4,16 @@ import  {ListItem} from '@material-ui/core';
 export default function Person(props) {
 const allUsers = props.allUsers
 const openProfile = props.openProfile
-
-console.log('This is all users:', allUsers)
-
-const result = allUsers.map(user => {
-  let id = user.id
-  let firstName = user.first_name
-  let lastName = user.last_name
-  let rank = user.rank
-  let jobTitle = user.job_title
-  return (<ListItem onClick={openProfile} id={id}> {`${rank} ${firstName} ${lastName} ${jobTitle}`} </ListItem>)
-
-  })
-  console.log('This is result', result)
+console.log('All users:', allUsers)
   return (
-    //Almost finished
-    <div>hello world</div>
-  );
+    <>
+      {allUsers.map((user) => {
+        console.log('User:', user)
+        return (
+          <ListItem onClick={openProfile} id={user.id}> {`${user.rank} ${user.first_name} ${user.last_name}`} </ListItem>
+        )
+      })
+      }
+    </>
+  )
 }
